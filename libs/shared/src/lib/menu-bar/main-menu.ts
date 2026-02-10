6e70f7e77c984f88bd485badf3b14edf19e2f858
Subhash Jha <subhashjha35@gmail.com>
2026-02-10 11:01:19 +0100

    Add main menu component with navigation and tests

---
+++ libs/shared/src/lib/menu-bar/main-menu.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'shared-main-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './main-menu.html',
+  styleUrl: './main-menu.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainMenu {}
--- libs/shared/src/public-api.ts
+++ libs/shared/src/public-api.ts
@@ -20,3 +20,4 @@ export {
 } from './lib/utils';
 export * from './lib/expandable-container';
 export * from './lib/mock';
+export * from './lib/menu-bar';
