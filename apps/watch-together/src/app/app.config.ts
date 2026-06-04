40b21a05efe4b1fbe613cd2598ef7eefb70e2e6d
Subhash Jha <subhashjha35@gmail.com>
2026-06-04 17:00:05 +0200

    updating to Angular 22

--- apps/watch-together/src/app/app.config.ts
+++ apps/watch-together/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXhr } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideHttpClient(),
+        provideHttpClient(withXhr()),
         provideRouter(appRoutes)
     ]
 };
